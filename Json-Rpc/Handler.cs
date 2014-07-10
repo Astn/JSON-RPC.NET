5d8c1156ac034098f437a21c669336930fbfa732
jakier <jakier@msn.com>
2014-07-10 13:54:21 +0900

    fix issue#25 error when calling method with nullable DateTime parameter

--- Json-Rpc/Handler.cs
+++ Json-Rpc/Handler.cs
@@ -390,7 +390,7 @@ using Newtonsoft.Json.Linq;
         private object CleanUpParameter(object p, SMDAdditionalParameters metaData)
         {
             var bob = p as JValue;
-            if (bob != null && (bob.Value == null || bob.Value.GetType() == metaData.ObjectType))
+            if (bob != null && (bob.Value == null || bob.Value.GetType() == metaData.ObjectType || bob.Type == JTokenType.Date))
             {
                 return bob.Value;
             }
