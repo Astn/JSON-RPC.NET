e58f5d6eb8e6a0ce1bfce5c082c8b9a29f65d96b
rommar <rommar@users.noreply.github.com>
2016-01-31 01:58:34 +0300

    Possibility to specify a method parameter name as it will be referred to by JsonRpc

--- Json-Rpc/Attributes.cs
+++ Json-Rpc/Attributes.cs
@@ -24,4 +24,27 @@ namespace AustinHarris.JsonRpc
             get { return jsonMethodName; }
         }
     }
+
+    /// <summary>
+    /// Used to assign JsonRpc parameter name to method argument.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Parameter, Inherited = false, AllowMultiple = false)]
+    public sealed class JsonRpcParamAttribute : Attribute
+    {
+        readonly string jsonParamName;
+
+        /// <summary>
+        /// Used to assign JsonRpc parameter name to method argument.
+        /// </summary>
+        /// <param name="jsonParamName">Lets you specify the parameter name as it will be referred to by JsonRpc.</param>
+        public JsonRpcParamAttribute(string jsonParamName = "")
+        {
+            this.jsonParamName = jsonParamName;
+        }
+
+        public string JsonParamName
+        {
+            get { return jsonParamName; }
+        }
+    }
 }
